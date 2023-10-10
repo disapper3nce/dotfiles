a95d322cb1b90be2910c4c61e42140df7a962bc6
disapper3nce <138326120+disapper3nce@users.noreply.github.com>
2023-10-11 04:10:00 +0800

    Add files via upload

---
+++ nvim~/init.lua
@@ -0,0 +1,11 @@
+require("plugs")
+require("core")
+require("ui")
+if vim.g.neovide then
+vim.g.neovide_padding_top = 39
+vim.g.neovide_padding_bottom = 39
+vim.g.neovide_padding_right = 39
+vim.g.neovide_padding_left = 39
+vim.o.guifont = "scientifica:h14" -- text below applies for VimScript
+
+end
